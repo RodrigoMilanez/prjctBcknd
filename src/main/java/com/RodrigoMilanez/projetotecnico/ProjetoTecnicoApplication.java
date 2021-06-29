3b4e6f7a080e4dce2420adbbc7fbc3f73fcddaeb
Rodrigo Milanez <rodrigomohland@hotmail.com>
2021-06-29 11:10:16 -0300

    Criação do projeto

---
+++ src/main/java/com/RodrigoMilanez/projetotecnico/ProjetoTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.RodrigoMilanez.projetotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RodrigoMilanez/projetotecnico/ProjetoTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RodrigoMilanez.projetotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
