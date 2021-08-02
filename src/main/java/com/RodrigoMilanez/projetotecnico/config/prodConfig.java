dcfdeba09ffb8b0b540eb8eb39f207ed32b00a3d
Rodrigo Milanez <rodrigomohland@hotmail.com>
2021-08-02 08:28:01 -0300

    bean email

--- src/main/java/com/RodrigoMilanez/projetotecnico/config/prodConfig.java
+++ src/main/java/com/RodrigoMilanez/projetotecnico/config/prodConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.RodrigoMilanez.projetotecnico.services.DBService;
+import com.RodrigoMilanez.projetotecnico.services.EmailService;
+import com.RodrigoMilanez.projetotecnico.services.MockMailService;
 
 @Configuration
 @Profile("prod")
@@ -20,6 +22,12 @@ public class prodConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		DBS.instantiateDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService(){
+		return new MockMailService();
+		 
 	}
 }
 
